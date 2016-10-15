ad8707482fd5e1343d94c0e2a92e5d9e2f1b5e82
Antoni Boucher <bouanto@zoho.com>
2016-10-14 23:25:02 -0400

    Added DOMDOMWindow::get_frame_element().

--- src/dom_dom_window.rs
+++ src/dom_dom_window.rs
@@ -1,11 +1,18 @@
+use std::ffi::CString;
+use std::ptr::null_mut;
+
+use ffi;
 use glib::IsA;
 use glib::translate::{ToGlibPtr, from_glib_full, from_glib_none};
+use gobject_sys::g_object_get;
+use libc::c_void;
 
 use super::{DOMCSSStyleDeclaration, DOMDOMWindow, DOMElement};
 use dom_dom_selection::DOMDOMSelection;
 
 pub trait DOMDOMWindowExtManual {
     fn get_computed_style<T: IsA<DOMElement>>(&self, element: &T, pseudo_element: Option<&str>) -> Option<DOMCSSStyleDeclaration>;
+    fn get_frame_element(&self) -> Option<DOMElement>;
     fn get_selection(&self) -> Option<DOMDOMSelection>;
 }
 
@@ -14,6 +21,17 @@ impl DOMDOMWindowExtManual for DOMDOMWindow {
         unsafe {
             from_glib_full(sys::webkit_dom_dom_window_get_computed_style(self.to_glib_none().0, element.to_glib_none().0, pseudo_element.to_glib_none().0))
         }
+    }
+
+    fn get_frame_element(&self) -> Option<DOMElement> {
+        let property_name = CString::new("frame-element").unwrap();
+        let mut value: *mut ffi::WebKitDOMElement = null_mut();;
+        let window: *mut ffi::WebKitDOMDOMWindow = self.to_glib_none().0;
+        unsafe {
+            g_object_get(window as *mut _, property_name.as_ptr(), &mut value as *mut _, null_mut() as *mut c_void);
+        }
+        let frame_element = unsafe { from_glib_none(value) };
+        frame_element
     }
 
     fn get_selection(&self) -> Option<DOMDOMSelection> {
