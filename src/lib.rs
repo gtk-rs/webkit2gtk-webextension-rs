41d37b772377e98f00e4e2578c05efcc455b535d
Antoni Boucher <bouanto@zoho.com>
2022-05-17 08:26:09 -0400

    Don't check in the GTK main loop is initialized because webextensions start with a glib loop already initialized anyway

--- src/lib.rs
+++ src/lib.rs
@@ -14,13 +14,6 @@ pub use glib::{Error, Object};
 
 macro_rules! assert_initialized_main_thread {
     () => {
-        if !::gtk::is_initialized_main_thread() {
-            if ::gtk::is_initialized() {
-                panic!("GTK may only be used from the main thread.");
-            } else {
-                panic!("GTK has not been initialized. Call `gtk::init` first.");
-            }
-        }
     };
 }
 
